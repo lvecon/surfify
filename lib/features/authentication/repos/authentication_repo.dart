1cf42259336b6ed48396cd343576b11dfc11a6ce
Namhyeon Heo <nammath11@gmail.com>
2023-03-29 01:42:25 +0900

    modify google login for android

--- lib/features/authentication/repos/authentication_repo.dart
+++ lib/features/authentication/repos/authentication_repo.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticaitonRepository {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   bool get isLoggedIn => user != null;
   User? get user => _firebaseAuth.currentUser;
 
   Future<void> googleSignIn() async {
-    await _firebaseAuth.signInWithProvider(GoogleAuthProvider());
+    await _googleSignIn.signIn();
   }
 }
 
