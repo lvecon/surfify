bec10e2511a33dd680a58be86a4b0cfbc1a34be6
Namhyeon Heo <nammath11@gmail.com>
2023-04-12 11:08:16 +0900

    add randome mode ui

--- lib/features/video/widgets/video_post.dart
+++ lib/features/video/widgets/video_post.dart
@@ -42,6 +42,8 @@ class _VideoPostState extends State<VideoPost>
 
   var radarMode = true;
 
+  bool randomMode = false;
+
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
       if (_videoPlayerController.value.duration ==
@@ -121,6 +123,8 @@ class _VideoPostState extends State<VideoPost>
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return VisibilityDetector(
       key: Key("${widget.index}"),
       onVisibilityChanged: _onVisibilityChanged,
@@ -262,6 +266,46 @@ class _VideoPostState extends State<VideoPost>
               ],
             ),
           ),
+          randomMode
+              ? Container()
+              : Positioned(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: size.width,
+                        height: 160,
+                        decoration: BoxDecoration(
+                          color: Colors.red.withOpacity(0.5),
+                        ),
+                      ),
+                      Container(
+                        width: size.width,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.red.withOpacity(0.7),
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                'Lucky Mode',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size18,
+                                ),
+                              ),
+                              Gaps.v8,
+                              Text('500m이내 무작위(50m 이동 시 새로 고침)',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: Sizes.size18,
+                                  )),
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
           Positioned(
             top: 50,
             right: 20,
@@ -298,7 +342,7 @@ class _VideoPostState extends State<VideoPost>
             top: 90,
             left: 20,
             child: VideoLocation(),
-          )
+          ),
         ],
       ),
     );
