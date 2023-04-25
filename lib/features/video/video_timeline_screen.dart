926c1b4207e126632b2b278403a56aff6777e045
Namhyeon Heo <nammath11@gmail.com>
2023-04-25 10:07:47 +0900

    add pizza video

--- lib/features/video/video_timeline_screen.dart
+++ lib/features/video/video_timeline_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:surfify/features/video/view_models/timeline_view_model.dart';
 import 'package:surfify/features/video/widgets/video_post.dart';
 
-class VideoTimelineScreen extends StatefulWidget {
+class VideoTimelineScreen extends ConsumerStatefulWidget {
   const VideoTimelineScreen({super.key});
 
   @override
-  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+  createState() => VideoTimelineScreenState();
 }
 
-class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+class VideoTimelineScreenState extends ConsumerState<VideoTimelineScreen> {
   int _itemCount = 4;
 
   final PageController _pageController = PageController();
@@ -62,28 +64,84 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     );
   }
 
+  List<String> video_index = [
+    "assets/videos/pizza02.mp4",
+    "assets/videos/pizza01.mp4",
+    "assets/videos/pizza03.mp4"
+  ];
+  List<String> video_index2 = [
+    "assets/videos/pizza02.mp4",
+    "assets/videos/pizza01.mp4",
+    "assets/videos/pizza03.mp4"
+  ];
+
+  List<Map<String, dynamic>> video_example = [
+    {
+      'src': "assets/videos/pizza02.mp4",
+      'nickname': "마곡드래곤(@dragmag)",
+      'content': '피자집',
+      'location': "비바 나폴리",
+      'longitude': 0,
+      'latitude': 0,
+      'comments': 0,
+    },
+    {
+      'src': "assets/videos/pizza01.mp4",
+      'location': "비바 나폴리",
+      'nickname': "마곡드래곤(@dragmag)",
+      'content': '피자집',
+      'longitude': 0,
+      'latitude': 0,
+      'comments': 0,
+    },
+    {
+      'src': "assets/videos/pizza03.mp4",
+      'location': "비바 나폴리",
+      'nickname': "마곡드래곤(@dragmag)",
+      'content': '피자집',
+      'longitude': 0,
+      'latitude': 0,
+      'comments': 0,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: _onRefresh,
-      displacement: 50,
-      edgeOffset: 20,
-      color: Theme.of(context).primaryColor,
-      child: PageView.builder(
-        controller: _pageController,
-        scrollDirection: Axis.vertical,
-        onPageChanged: _onPageChanged,
-        itemCount: _itemCount,
-        itemBuilder: (context, index) => PageView.builder(
-          controller: _pageController2,
-          scrollDirection: Axis.horizontal,
-          onPageChanged: _onPageChanged2,
-          itemCount: _itemCount,
-          itemBuilder: (context, index) => VideoPost(
-            index: index,
-          ),
-        ),
-      ),
-    );
+    return ref.watch(timelineProvider).when(
+        loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+        error: (error, stackTrace) => Center(
+              child: Text('Could not load videos: $error'),
+            ),
+        data: (videos) => RefreshIndicator(
+              onRefresh: _onRefresh,
+              displacement: 50,
+              edgeOffset: 20,
+              color: Theme.of(context).primaryColor,
+              child: PageView.builder(
+                controller: _pageController,
+                scrollDirection: Axis.vertical,
+                onPageChanged: _onPageChanged,
+                itemCount: _itemCount,
+                itemBuilder: (context, index) => PageView.builder(
+                  controller: _pageController2,
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: _onPageChanged2,
+                  itemCount: _itemCount,
+                  itemBuilder: (context, index) => (index == 0)
+                      ? VideoPost(
+                          index: index,
+                          src: video_index[index],
+                          nickname: video_example[index]['nickname'],
+                          content: video_example[index]['content'])
+                      : VideoPost(
+                          index: index,
+                          src: video_example[index]['src'],
+                          nickname: video_example[index]['nickname'],
+                          content: video_example[index]['content']),
+                ),
+              ),
+            ));
   }
 }
