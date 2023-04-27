4e883b3faee18517c06cff339593a0926aee0baf
Namhyeon Heo <nammath11@gmail.com>
2023-04-28 01:33:10 +0900

    upload video and make thumbnail

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin"
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+export const onVideoCreated = functions.firestore
+.document("videos/{videoId}")
+.onCreate(async (snapshot, context) =>{
+    const spawn = require("child-process-promise").spawn;
+     const video = snapshot.data();
+     await spawn("ffmpeg", [
+       "-i",
+       video.fileUrl,
+       "-ss",
+       "00:00:01.000",
+       "-vframes",
+       "1",
+       "-vf",
+       "scale=150:-1",
+       `/tmp/${snapshot.id}.jpg`,
+     ]);
+     const storage = admin.storage();
+     const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+       destination: `thumbnails/${snapshot.id}.jpg`,
+     });
+     await file.makePublic();
+     await snapshot.ref.update({thumbnailUrl: file.publicUrl()});
+
+     const db = admin.firestore();
+
+     await db.collection('users').doc(video.creatorUid).collection("videos").doc(snapshot.id).set({
+        thumbnailUrl: file.publicUrl(),
+        videoId: snapshot.id,
+     });
+});
