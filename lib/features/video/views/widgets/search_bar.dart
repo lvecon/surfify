0e0d60e56d6b5bcf6f3f1cf053c24ad83781e635
OctoHoon <sakonghoon@naver.com>
2023-05-12 23:06:27 +0900

    searchBar Provider

--- lib/features/video/views/widgets/search_bar.dart
+++ lib/features/video/views/widgets/search_bar.dart
@@ -5,7 +5,11 @@ import '../../../../constants/gaps.dart';
 import '../../../../constants/sizes.dart';
 
 class SearchBar extends StatelessWidget {
-  const SearchBar({super.key});
+  final List<String> searchcondition;
+  const SearchBar({
+    super.key,
+    required this.searchcondition,
+  });
 
   final searched = false;
 
@@ -21,20 +25,57 @@ class SearchBar extends StatelessWidget {
         padding: const EdgeInsets.all(6.0),
         child: Row(
           mainAxisSize: MainAxisSize.max,
-          children: const [
-            FaIcon(
+          children: [
+            const FaIcon(
               FontAwesomeIcons.magnifyingGlass,
               size: Sizes.size18,
               color: Colors.white,
             ),
             Gaps.h6,
-            Text(
-              '주변 모든 장소 서핑',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-              ),
-            ),
+            searchcondition.isEmpty
+                ? const Text(
+                    '주변 모든 장소 서핑',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  )
+                : SizedBox(
+                    child: Wrap(children: [
+                      for (var keyword in searchcondition)
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                16,
+                              ),
+                              color: keyword.toString().contains('@')
+                                  ? Theme.of(context)
+                                      .primaryColor
+                                      .withOpacity(0.7)
+                                  : Colors.red.withOpacity(0.7),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    keyword,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Gaps.h6,
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                    ]),
+                  ),
           ],
         ),
       ),
