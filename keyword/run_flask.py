f65769b0cf8cd9b5abffd9c9bf93fcbd91d448df
muffinsnip <msyoon418@gmail.com>
2023-06-08 00:27:28 +0900

    Divide labels to filtering and tagging for hierarchical inference. Move translations to flask server. Change hasViolence to isFlagged in app.

--- keyword/run_flask.py
+++ keyword/run_flask.py
@@ -9,10 +9,23 @@ import clip
 import torch
 import logging
 import time
+import yaml
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# model, preprocess = clip.load("./ViT-B-32.pt", device=device)
 model, preprocess = clip.load("ViT-B/32", device=device)
+
+with open('./translation.yaml','r') as f:
+    translation = yaml.safe_load(f)
+print(translation)
+
+filtering_labels = []
+with open('filtering_labels.txt', 'r') as f:
+    filtering_labels = [line.strip() for line in f.readlines()]
+
+
+tagging_labels = []
+with open('tagging_labels.txt', 'r') as f:
+    tagging_labels = [line.strip() for line in f.readlines()]
 
 app = Flask(__name__)
 
@@ -50,48 +63,72 @@ def sample_frames(video_filepath):
 
     return returning_frames
 
-def extract_keyword(images, labels):
-    # hashtags = [f"#{label.split()[-1]}" for label in labels]
-    images = [preprocess(image) for image in images]
-    image_input = torch.tensor(np.stack(images))
-
-    image_features = model.encode_image(image_input).float()
-    text_tokens = clip.tokenize(labels)
-
-    with torch.no_grad():
-        image_features = model.encode_image(image_input).float()
-        text_features = model.encode_text(text_tokens).float()
-
+def should_filter(image_features, text_features):
     image_features /= image_features.norm(dim=-1, keepdim=True)
     text_features /= text_features.norm(dim=-1, keepdim=True)
     text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
     top_probs, top_labels = text_probs.topk(5, dim=-1)
 
-    returning_json = {"violence": 0, "labels": []}
-    predicted_label = []
-    found_violence = False
+    flag = 0
 
-    for i in range(len(images)):
+    for i in range(len(top_probs)):
         for j in range(len(top_probs[i])):
             if top_probs[i][j] > 0.2:
-                predicted_label.append((labels[top_labels[i][j]], f"{top_probs[i][j].item():.3f}"))
+                if 'violence' in filtering_labels[top_labels[i][j]] or \
+                   "This photo contains violence." in filtering_labels[top_labels[i][j]] or \
+                   "Beautiful Blonde babe gets fucked from behind" in filtering_labels[top_labels[i][j]] or \
+                   'Awesome fuck followed by cumming deep in her throat' in filtering_labels[top_labels[i][j]]:
+                    flag = 1
+    return flag
 
-                if 'violence' in labels[top_labels[i][j]] or "This photo contains violence." in labels[top_labels[i][j]]:
-                    found_violence = True
-                    break
+def get_tagging_predicted_labels(image_features, text_features):
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    top_probs, top_labels = text_probs.topk(5, dim=-1)
 
-    # first image contains the stable image
-    if found_violence:
-        returning_json["violence"] = 1
+    predicted_labels = []
+    for i in range(len(top_probs)):
+        for j in range(len(top_probs[i])):
+            if top_probs[i][j] > 0.2:
+                predicted_labels.append((tagging_labels[top_labels[i][j]], top_probs[i][j].item()))
+
+    predicted_labels.sort(key=lambda x: x[1], reverse=True)
+    return predicted_labels
+
+def extract_keyword(images):
+    images = [preprocess(image) for image in images]
+
+    image_input = torch.tensor(np.stack(images))
+    filtering_text_tokens = clip.tokenize(filtering_labels)
+    with torch.no_grad():
+        image_features = model.encode_image(image_input).float()
+        text_features = model.encode_text(filtering_text_tokens).float()
+
+    returning_json = {"flag": 0, "labels": []}
+    flag = should_filter(image_features, text_features)
+
+    if flag:
+        returning_json["flag"] = 1
     else:
-        predicted_label.sort(key=lambda x: x[1], reverse=True)
+        tagging_text_tokens = clip.tokenize(tagging_labels)
+        with torch.no_grad():
+            text_features = model.encode_text(tagging_text_tokens).float()
+
+        predicted_labels = get_tagging_predicted_labels(image_features, text_features)
+        print(predicted_labels)
+        predicted_set = set()
         # select the top 5 non-overlapping labels with the highest probability
-        for i in range(len(predicted_label)):
+        for i in range(len(predicted_labels)):
             if len(returning_json["labels"]) >= 5:
                 break
-            if predicted_label[i][0] not in returning_json["labels"]:
-                returning_json["labels"].append(predicted_label[i][0])
-
+            if predicted_labels[i][0] not in predicted_set:
+                try:
+                    returning_json["labels"].append(translation[predicted_labels[i][0]])
+                except KeyError:
+                    returning_json["labels"].append(predicted_labels[i][0])
+                predicted_set.add(predicted_labels[i][0])
+    print(returning_json)
     return returning_json
 
 @app.route("/health")
@@ -101,9 +138,6 @@ def health():
 
 @app.route('/predict_video', methods=['POST'])
 def predict_video():
-    labels = []
-    with open('labels.txt', 'r') as f:
-        labels = [line.strip() for line in f.readlines()]
     # get video from request
     video_file = request.files['video']
     app.logger.info(f"torch running on {device}")
@@ -115,15 +149,18 @@ def predict_video():
         start_time = time.time()
         frames = sample_frames(video_filepath)
         end_time = time.time()
+
         app.logger.info(f"{end_time - start_time} for frame sampling")
         # opencv uses BGR by default, change to RGB for PIL
         frames = [cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) for frame in frames]
         pil_images = [Image.fromarray(frame) for frame in frames]
 
         start_time = time.time()
-        keywords = extract_keyword(pil_images, labels)
+        keywords = extract_keyword(pil_images)
         end_time = time.time()
+
         app.logger.info(f"{end_time - start_time} for keyword extraction")
+
         return jsonify(keywords)
     return jsonify([])
 
@@ -132,10 +169,8 @@ def predict_image():
     image = [Image.open(request.files['image'])]
     app.logger.info(f"torch running on {device}")
     app.logger.info("FLASK HAS RECEIVED THE IMAGE")
-    with open('labels.txt') as f:
-        keywords = [x.strip() for x in f.readlines()]
 
-    keywords = extract_keyword(image, keywords)
+    keywords = extract_keyword(image)
     return jsonify(keywords)
 
 if __name__ == "__main__":
